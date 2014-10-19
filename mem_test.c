36faafd5a359bfbec7e23eed4de4640a21a73bb1
akai <akai@a125040.npic.edu.tw>
2014-10-19 15:36:38 +0800

    update

--- mem_test.c
+++ mem_test.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define test
+
 #define max(a,b) \
 	a->index>b->index?a:b
 
-#define min(a,b) max(a,b)!=a?a:b
+#define min(a,b) \
+	a->index<b->index?a:b
 
 typedef struct list{
 	unsigned long int index;
@@ -13,26 +16,32 @@ typedef struct list{
 }list;
 
 inline void link(list *a,list *b){a->next = b;};
+inline int re(list *a,list *b){if(a==b)return 1;else return 0;}
+
+#ifdef finish
+int merge_sort(list *l,int p,int q);
+int merge(list *l,int p,int q,int r);
+#endif
 
 int main(void)
 {	
 	srand(time(NULL));
-	unsigned long int num;
 	list *first;
-	//for(int i=0;i<10;i++)
-		first = (list *)malloc(sizeof(list *));
-	first->index = (rand()%1000000)+1;
+	list *search;
+	first = (list *)malloc(sizeof(list *));
+	first->index = 0;//rand()%1000000+1;
 	first->next = NULL;
+	
+	unsigned long int i=1;
 
-	while(1){
+	//insert and sort
+	while(i!=10000){
 		list *new;
-	//	for(int i=0;i<10;i++)	
-			new = (list *)malloc(sizeof(list *));
-		new->index = (rand()%1000000)+1;
+		new = (list *)malloc(sizeof(list *));
+		new->index = i;//rand()%1000000+1;
 		new->next = NULL;
-		
 
-		list *search = first;
+		search = first;
 		list *_next;
 		if(first->next!=NULL)
 			_next = search->next;
@@ -41,22 +50,91 @@ int main(void)
 				link(max(search,new),min(search,new));
 				break;
 			}
-			if(max(search,new)&&min(_next,new)){
+			if(re(search,max(search,new))&&re(_next,min(_next,new))){
 				link(search,new);
 				link(new,_next);
+				break;
+			}
+			if(re(new,max(new,search))){
+				link(new,search);
 				break;
 			}
 			search = search->next;
 			_next = _next->next;
 		}
+		first = max(search,new);
+		i++;
+	}
+#ifdef test
+		i=0;
+		search = first;
+		while(search!=NULL){
+			printf("%d\n",search->index);
+			search = search->next;
+			printf("i: %d\n",i++);
+		}
+#endif
 
-		first = max(first,new);
-		
-		//search = first;
-		//while(search!=NULL){
-		//	printf("%d\n",search->index);
-		//	search = search->next;
-		//}
+#ifdef finish
+	//use swap
+	while(1){
+		while(first->next!=NULL){
+			
+		}
+			
+	}
+#endif
+}
 
+#ifdef finish
+int merge_sort(list *l;int p,int r){
+	if(p<r){
+		int q = (p+r)/2;
+		merge_sort(l,p,q);
+		merge_sort(l,q+1,r);
+		merge(l,p,q,r);
+	}
+
+}
+
+int merge(list *l,int p,int q,int r){
+	int n1 = q-p+1;
+	int n2 = r-q;
+
+	
+
+}
+#endif
+
+
+#ifdef finish
+int partition(int p,int r){
+	int x,i,j,k;
+
+	x=A[r];
+	i=p-1;
+	for (j=p;j<=r-1;j++){
+		if (A[j]<=x){
+			i++;
+			k=A[i];
+			A[i]=A[j];
+			A[j]=k;
+		}
+	}
+	k=A[i+1];
+	A[i+1]=A[r];
+	A[r]=k;
+	return i+1;
+}
+
+void quicksort(int p,int r){
+	int q;
+
+	if (p<r){
+	   q=partition(p,r); 
+	   quicksort(p,q-1);
+	   quicksort(q+1,r);	
 	}
 }
+#endif
+
